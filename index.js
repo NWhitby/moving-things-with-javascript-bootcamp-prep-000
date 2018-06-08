87dd02d57a1ff2909c362b8a809b6632d84cbca6
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-08 21:00:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const app = "I don't do much."
 
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace('px', '')
+  let right = parseInt(righttNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
