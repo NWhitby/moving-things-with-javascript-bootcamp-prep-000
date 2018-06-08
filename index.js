309420e73cb9cf442d0c2e629239cd6183700885
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-08 21:11:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 const app = "I don't do much."
 
-let dodger = document.getElementById('dodger')
-
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace('px', '')
-  let left = parseInt(leftNumbers, 10)
-
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`
-  }
-}
-
-function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace('px', '')
-  let right = parseInt(rightNumbers, 10)
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`
-  }
-}
-
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-  if (e.which === 39) {
-    moveDodgerRight()
-  }
+  document.addEventListener('keydown', function(e) {
+      if (e.which === 37) {
+        moveDodgerLeft()
+      }
+      if (e.which === 39) {
+        moveDodgerRight()
+      }
+    })
+    function moveDodgerLeft() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+      if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+      }
+    }
+    function moveDodgerRight() {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+      if (left < 360) {
+        dodger.style.left = `${left + 1}px`
+      }
+    }
